8d054dce62fb092fb9a0f9d8a066b597718e8a47
KaShTaN27 <dkachan.lida@mail.ru>
2022-11-28 11:00:46 +0300

    Add login dto

---
+++ src/main/java/by/bsuir/lab3/dto/LoginDto.java
@@ -0,0 +1,13 @@
+package by.bsuir.lab3.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginDto {
+    private String login;
+    private String password;
+}
