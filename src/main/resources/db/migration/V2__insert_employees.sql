29e5a460a9433ea5ab8756e50c54eb5b6c1e14a9
KaShTaN27 <dkachan.lida@mail.ru>
2022-11-28 11:04:40 +0300

    Encrypt employees passwords

--- src/main/resources/db/migration/V2__insert_employees.sql
+++ src/main/resources/db/migration/V2__insert_employees.sql
@@ -1,4 +1,4 @@
 INSERT INTO employee (email, name, password, role)
-VALUES ('john@gmail.com', 'John', '123123223', 'VIEWER'),
-       ('adrian@gmail.com', 'Adrian', '4625216', 'CREATOR'),
-       ('gleb@gmail.com', 'Gleb', '43622552', 'ADMIN');
+VALUES ('john@gmail.com', 'John', '$2a$12$CBAql.V9iOuRJlI/zIhpuuD2CJTEu6TMdDYvzBcbWmR70E.HvWLeC', 'VIEWER'),
+       ('adrian@gmail.com', 'Adrian', '$2a$12$lj2p4at2lP3LUAJ0DM28e.A6QZtgUm0W4/03s6aOo9gUSzuxnqIJm', 'CREATOR'),
+       ('gleb@gmail.com', 'Gleb', '$2a$12$BIrZJbEdIYQL.pG8/evbqOQVo6tI32CcGT34YU.cCtuzx.7PCI0W6', 'ADMIN');
